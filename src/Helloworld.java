8764c7ed1330226b9fb6a32085c8ce3d9b8ebe8e
Xacarami <gabriel.plautz2018@gmail.com>
2022-04-24 01:01:15 -0300

    first java commit.

---
+++ src/Helloworld.java
@@ -0,0 +1,17 @@
+import br.com.dio.model.Gato;
+
+public class Helloworld {
+	public static void main(String args[]) {
+
+		Gato gato = new Gato(null, null, null);
+
+		System.out.println(gato);
+		
+		
+		/*int a = 2;
+		int b = 3;
+		int c = a + b;
+		System.out.println("Até a Lua! " + c + gato);*/
+
+	}
+}
